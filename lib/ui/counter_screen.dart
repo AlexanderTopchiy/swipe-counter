41fd66857cceff5c2d12cf84f329d18e3acbe444
Alexander Topchiy <topchiyau@gmail.com>
2019-11-27 22:22:15 +0300

    Add a counter card and directions markers

--- lib/ui/counter_screen.dart
+++ lib/ui/counter_screen.dart
@@ -6,7 +6,9 @@ class CounterScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: <Widget>[
+          // Background
           Container(color: Color.fromRGBO(0, 137, 123, 1)),
+          // Title
           Align(
             alignment: Alignment.topCenter,
             child: Container(
@@ -20,13 +22,14 @@ class CounterScreen extends StatelessWidget {
               ),
             ),
           ),
+          // Reset button
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              margin: EdgeInsets.only(bottom: 100.0),
+              margin: EdgeInsets.only(bottom: 80.0),
               child: ButtonTheme(
-                height: 40.0,
-                minWidth: 150.0,
+                height: 42.0,
+                minWidth: 140.0,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)
                 ),
@@ -40,6 +43,67 @@ class CounterScreen extends StatelessWidget {
                       ),
                     ),
                   onPressed: () {},
+                ),
+              ),
+            ),
+          ),
+          // Decrement counter direction
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(255, 255, 255, 0.4),
+              ),
+              height: 30.0,
+              width: 30.0,
+              margin: EdgeInsets.only(left: 10.0),
+              child: Center(
+                child: Text(
+                  '-', 
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0
+                    ),
+                ),
+              ),
+            ),
+          ),
+          // Increment counter direction
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(255, 255, 255, 0.4),
+              ),
+              height: 30.0,
+              width: 30.0,
+              margin: EdgeInsets.only(right: 10.0),
+              child: Center(
+                child: Text(
+                  '+', 
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0
+                    ),
+                ),
+              ),
+            ),
+          ),
+          // Counter card
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              height: 200.0,
+              width: 200.0,
+              child: Center(
+                child: Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 50.0
+                  ),
                 ),
               ),
             ),
